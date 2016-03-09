81b97488dd57075b3a2acb2a4009ceb7f0ea531d
Jerome Etienne <jerome.etienne@gmail.com>
2016-03-09 17:41:42 +0000

    fixing the trackcontrols missing js

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ Main.prototype._previewCtor	= function(content)
 	baseUrl		= baseUrl.replace(window.location.hash, '');
 	content	= content.replace(/src="/g	, "src=\""	+baseUrl + "./data/boilerplate.orig/")
 	content	= content.replace(/href="/g	, "href=\""	+baseUrl + "./data/boilerplate.orig/")
-	console.log("content", content);
+	// console.log("content", content);
 	// build the data url itself
 	var url = "data:text/html;base64,"+window.btoa(content);
 	// create the iframe for the preview
